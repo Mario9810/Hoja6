ca940c9ebf94cdda44d62bbd202edb6ce76f849f
DESKTOP-PO2B61U\cooli <arg17801@uvg.edu.gt>
2018-03-09 10:47:49 -0600

    Cambios

---
+++ Hoja6/src/hoja6/BuscadorCarta.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja6;
+
+/**
+ *
+ * @author cooli
+ */
+public class BuscadorCarta {
+    private FactoryHash buscador;
+    
+    public BuscadorCarta(){
+        
+    }
+    
+    public String buscar(){
+        return;
+    }
+}
