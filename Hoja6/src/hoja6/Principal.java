9a64608ad0974078fe02d95dcb77d816efef2ea2
DESKTOP-VMUPS6B\massa <hmmyfh@gmail.com>
2018-03-08 21:37:27 -0600

    Principal añadido

---
+++ Hoja6/src/hoja6/Principal.java
@@ -0,0 +1,19 @@
+
+public class Principal{
+	 public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		try {
+            Scanner input = new Scanner(file);
+            while(input.hasNext())
+            {
+             
+           
+            }
+            input.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        } 
+        
+	 }
+	 
+}
